d791e9d561211783df0fef2b7126683855955f56
Chaehan So <chaehan.so@gmail.com>
2019-02-08 23:44:20 +0900

    final fix target.label > target_label in model.permutation.list as argument to train_model_permutations() | cleaned Scribble

--- psy-data-analysis.R
+++ psy-data-analysis.R
@@ -17,7 +17,7 @@ dataset.label <- paste0(c("data/dataset", "rds"), collapse = ".")
 
 # load libraries
 # devtools::install_github("agilebean/machinelearningtools", force = TRUE)
-libraries <- c("dplyr", "magrittr", "tidyverse", "purrr"
+libraries <- c("dplyr", "magrittr", "tidyverse",
                , "sjlabelled" # read SPSS
                , "caret", "doParallel"
                , "stargazer", "DataExplorer", "skimr"
@@ -26,11 +26,11 @@ libraries <- c("dplyr", "magrittr", "tidyverse", "purrr"
 )
 sapply(libraries, require, character.only = TRUE)
 
-target.label.list <- c("PERF09", "PERF.all")
+target.label.list <- c("PERF09", "PERF.all", "TO.all")
 features.set.list <- c("big5items", "big5composites")
 
-model.permutations.list <- crossing(target.label = target.label.list, 
-                                    features.set = features.set.list)
+model.permutations.list <- crossing(target_label = target.label.list, 
+                                    features_set = features.set.list)
 
 # nominal <- FALSE # with ordinal as ORDERED factors
 nominal <- TRUE # with ordinal as NOMINAL factor
@@ -64,14 +64,10 @@ get_features <- function(target_label, features_set, data_set) {
 #######################################################################
 # TRAIN model permutations
 #######################################################################
-train_model_permutations <- function(target.label, features.set, 
+train_model_permutations <- function(target_label, features_set, 
                                      data_set, algorithm_list, training_configuration, 
                                      seed = 17, split_ratio = 0.80, try_first = NULL
                                      ) {
-  # emphasize variables are passed arguments
-  target_label <- target.label
-  features_set <- features.set
-  
   # define output filename
   models.list.name <- paste0(c("data/models.list", target_label, features_set, "rds"), 
                              collapse = ".") %T>% print
@@ -156,6 +152,7 @@ algorithm.list <- c(
   # , "svmRadial"
 )
 
+
 #######################################################################
 # MAIN
 #######################################################################
@@ -168,8 +165,8 @@ if (mode == "new") {
       pmap(train_model_permutations, 
            data_set = dataset,
            algorithm_list = algorithm.list,
-           training_configuration = trainControl(method = "repeatedcv", number = 10, repeats = 3), 
-           try_first = 50
+           training_configuration = trainControl(method = "repeatedcv", number = 10, repeats = 10), 
+           try_first = NULL
       )
   ) %>% print
 
@@ -191,30 +188,25 @@ if (mode == "new") {
 ## 4.1 Training Set Performance
 ########################################
 # get model
-models.list <- get_models_list(model.permutations.list, 1)
+models.list <- get_models_list(model.permutations.list, 4)
 
 # training set performance
 models.metrics <- models.list %>% get_model_metrics %T>% print
 
 # set color palettes (default = "Set1")
-# models.list %>% get_model_metrics(palette = "Accent")
 models.list %>% get_model_metrics(palette = "Dark2")
 
 # get model comparison
 models.list %>% head(-2) %>% resamples %>% dotplot
-# models.list %>% resamples %>% bwplot
+models.list %>% head(-2) %>% resamples %>% bwplot
 
 ########################################
 ## 4.2 Testing Set Performance
 ########################################
 # RMSE for all models on testing set
-models.list %>%
-  get_model_metrics %>%
-  .$RMSE.testing %>% kable(caption = "testing set performance: RMSE")
-
-models.list %>%
-  get_model_metrics %>%
-  .$RMSE.all %>% kable(caption = "training vs. testing set performance: RMSE")
+models.metrics$RMSE.testing
+# training vs. testing set performance: RMSE
+models.metrics$RMSE.all 
 
 ################################################################################
 ################################################################################
@@ -225,87 +217,9 @@ models.list %>%
 ################################################################################
 ################################################################################
 
-# ggplot(data = testing.set, aes(x = PERF.all)) + geom_bar()
-# 
-# training.set$PERF.all %>% summary
-# testing.set$PERF.all %>% summary
-# 
-# # testing set performance with lm
-# predictions.best <- predict(models.list$lm, testing.set, na.action = na.pass)
-# rmse.best.testing <- sqrt(mean((predictions.best- testing.set[[target.label]])^2)) %>% print
-# rmse.best.training <- models.lists.all[[3]] %>% .$lm %>% .$results %>% .$RMSE %>% print
 
 
-# ################################################################################
-# # Compare preProcess outside/inside train()
-# ################################################################################
-# ## Compare preProcess outside/inside train()
-# # models.list.name <- "data/models.lists.3.PERF.all.rds"
-# # models.lists.all <- models.list.name %>% readRDS
-# # models.list <- models.lists.all[[3]]
-# 
-# if (models.list.name == "data/models.lists.3.PERF.all.rds") {
-#   models.lists.all <- "data/models.lists.3.PERF.all.rds" %>% readRDS
-#   
-#   ## models 1: preProcess outside train() 10-fold cv
-#   models.lists.all[[1]]$result
-#   
-#   ## models 2: preProcess inside train(), 10-fold cv
-#   models.lists.all[[2]]$result
-#   
-#   ## models 3: preProcess inside train(), 10-fold repeated cv
-#   models.lists.all[[3]]$result
-#   
-# }
 
-################################################################################
-# RMSE > MSE
-################################################################################
-# RMSE
-models.RMSE <- models.resamples %>% summary %>% .$statistics %>% .$RMSE %>% as.data.frame
-
-# ## RMSE - conserve model names in rownames
-# models.RMSE %>%
-#   # safer than subset(select = Mean)
-#   select(Mean) # only retains row names if no further action
-
-# MSE
-## MSE - add column rowname for model names + sort
-models.RMSE %>% 
-  tibble::rownames_to_column() %>%
-  mutate(model=rowname, RMSE=Mean, MSE = Mean^2) %>% 
-  select(model, RMSE, MSE) %>% 
-  arrange(RMSE)
-
-# ### alternative(RMSE - add column rowname for model names + sort)
-# models.RMSE %>% 
-#   mutate(RMSE=Mean, MSE = Mean^2) %>% 
-#   select(RMSE, MSE) %>% 
-#   arrange(RMSE) %>% 
-#   # retaining the row names requires matrix transformation
-#   as.matrix %>%
-#   matrix(nrow = nrow(models.RMSE), # ncol = 2,
-#          dimnames = list(dimnames(models.RMSE)[[1]], c("RMSE", "MSE")))
-
-## MSE
-### apply a function (square) to all elements of a dataframe
-models.MSE <- models.RMSE %>%
-  # square the RMSE
-  sapply(function(x) x^2) %>% 
-  ## tricky: preserve dataframe dimensions (matrix) AND model names (dimnames)
-  matrix(nrow = nrow(models.RMSE), dimnames = dimnames(models.RMSE)) %>% 
-  as.data.frame %T>% print
-
-# MSE boxplots - base r
-models.MSE %>% select(-`NA's`) %>% t %>% boxplot
-
-# MSE boxplots - ggplot
-models.MSE %>% select(-`NA's`) %>% t %>% as.data.frame %>% 
-  tidyr::gather(key = model, value = MSE) %>% 
-  ggplot(aes(x = reorder(model, desc(MSE), median), y = MSE)) +
-  geom_boxplot() +
-  # coord_flip() +
-  xlab("model")
 
 
 
