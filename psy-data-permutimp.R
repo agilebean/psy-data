6cd9bd72c274ecded6b0f38e86c8295f76b3d6fe
Chaehan So <chaehan.so@gmail.com>
2021-05-31 00:46:10 +0900

    implemented get_pimp_range_List_models_list, get_pimp_for_models_list(), save_pimp_plots()

--- psy-data-permutimp.R
+++ psy-data-permutimp.R
@@ -7,6 +7,9 @@
 # load libraries
 # detach("package:machinelearningtools", character.only = TRUE)
 # devtools::install_github("agilebean/machinelearningtools", force = TRUE)
+# library(credentials)
+# set_github_pat(force_new = FALSE, validate = interactive(), verbose = TRUE)
+
 libraries <- c(
   "magrittr"
   , "caret"
@@ -25,26 +28,122 @@ source("_common.R")
 
 getOption("digits")
 
-
 ################################################################################
-# get data of models.list
+# get permutation feature importance (pimp) for models.list
 ################################################################################
-get_models_varimp <- function(models_list) {
+get_pimp_for_models_list <- function(
+  models_list, no_permutations = 50, seed = 171) {
 
-  models_list %>%
-    names %>%
-    # tricky: avoid glmnet by squeezing ^lm$
-    str_detect("^RF|GBM|LR$") %>%
-    # select specific list elements by name
-    purrr::keep(models_list, .)
+
+
+  fi.list.DALEX <- models_list %>%
+
+    imap(function(model_object, model_name) {
+
+      # model_object <- models.list$SVM
+      print(paste("*********", model_object$method))
+
+      training.set <- model_object$trainingData %>%
+        select(.outcome, everything())
+
+      target <- training.set$.outcome
+      print(paste("***target"))
+
+      features <- training.set %>% select(-.outcome)
+
+      DALEX.explainer <- DALEX::explain(
+        model = model_object,
+        data = features,
+        y = target,
+        label = model_name, # .y in imap() is list element name
+        colorize = TRUE
+      )
+      print("*** DALEX.explainer")
+
+      set.seed(seed)
+      system.time(
+        DALEX.permutation.fi <- model_parts(
+          explainer = DALEX.explainer,
+          loss_function = loss_root_mean_square,
+          B = no_permutations,
+          type = "ratio"
+        )
+      ) # 78-84s for rf
+
+      print("*** DALEX.permutation.fi")
+
+      # bar_width = 5.9 items / 12.4 composites
+      ( bar.width <- 20 / log(ncol(features)) )
+
+      DALEX.permutation.fi.plot <- DALEX.permutation.fi %>%
+        plot(
+          bar_width = bar.width
+          , show_boxplots = FALSE
+          , title = "Permutation Feature importance"
+          , subtitle = ""
+        ) +
+        scale_y_continuous(expand = expansion()) # reduce space to axis
+
+      print("*** DALEX.permutation.fi.plot")
+
+      return(
+        list(
+          DALEX.explainer = DALEX.explainer
+          , DALEX.permutation.fi = DALEX.permutation.fi
+          , DALEX.permutation.fi.plot = DALEX.permutation.fi.plot
+        )
+      )
+    })
+  # 44s B=10, 220s B=50, 408s B=100
+
+  return(fi.list.DALEX)
 }
 
-################################################################################
-# MAIN: all models
-################################################################################
-# step1)
-# read models.lists from all datasets
+#####################################################
+# calculate permutation feature importance
+#####################################################
+get_pimp_range_List_models_list <- function(
+  list_models_list, model_string = "", value_function = max) {
 
+  list_models_list %>%
+    {
+      if (model_string != "") {
+
+        get_list_elements_by_string(., model_string)
+
+      } else {
+        .
+      }
+    } %>%
+    # if model_string, then a subset of models_list is used!
+    # VERY TRICKY: temporary assignment works only for . not names(.)
+    # TRICKY: get names() of models.selected later
+    {
+      {
+        . -> models.selected
+      } %>%
+        imap_dfr(function(models_list, models_list_label) {
+          models_list %>%
+            imap(function(model, model_label) {
+              model$DALEX.permutation.fi %>%
+                filter(variable != "_baseline_") %>%
+                group_by(variable) %>%
+                summarise(mean_dropout_loss = mean(dropout_loss)) %>%
+                .$mean_dropout_loss %>%
+                value_function
+            })
+        }) %>%
+        as.data.frame %>%
+      set_rownames(models.selected %>% names)
+    }
+}
+
+
+################################################################################
+# MAIN
+################################################################################
+# Step1) read models.lists from all datasets
+################################################################################
 # NEW <- TRUE
 NEW <- FALSE
 
@@ -66,86 +165,24 @@ if (NEW) {
 }
 
 datasets.models.list %>% names
-models.list <- datasets.models.list %>% pluck(1) # composites
-models.list <- datasets.models.list %>% pluck(5) # items
+# models.list <- datasets.models.list %>% pluck(1) # composites
+# models.list <- datasets.models.list %>% pluck(5) # items
 
-getOption("parallelly.fork.enable")
-options(parallelly.fork.enable = TRUE)
-plan(multicore)
-
-imap(models.list,
-     ~ paste(.y, .x$method))
-
-get_permutation_fi_for_models_list <- function(models_list) {
-
-  fi.list.DALEX <- models_list %>%
-
-    imap(function(model_object, model_name) {
-
-      print(paste("*********", model_object$method))
-
-      training.set <- model_object$trainingData %>%
-        select(.outcome, everything())
-
-      target <- training.set$.outcome
-      print(paste("***target"))
-
-      features <- training.set %>% select(-.outcome)
-
-      DALEX.explainer <- DALEX::explain(
-        model = model_object,
-        data = features,
-        y = target,
-        label = model_name, # .y in imap() is list element name
-        colorize = TRUE
-      )
-      print("*** DALEX.explainer")
-
-      set.seed(SEED)
-      system.time(
-        DALEX.permutation.fi <- model_parts(
-          explainer = DALEX.explainer,
-          loss_function = loss_root_mean_square,
-          B = 50,
-          type = "ratio"
-        )
-      ) # 78-84s for rf
-
-      print("*** DALEX.permutation.fi")
-
-      # bar_width = 5.9 items / 12.4 composites
-      ( bar.width <- 20 / log(ncol(features)) )
-
-      DALEX.permutation.fi.plot <- DALEX.permutation.fi %>%
-        plot(
-          bar_width = bar.width
-          , title = "Permutation Feature importance"
-          , subtitle = ""
-        ) +
-        scale_y_discrete(expand = c(0, 0)) # reduce space to axis
-      DALEX.permutation.fi.plot
-      print("*** DALEX.permutation.fi.plot")
-
-      return(
-        list(
-          DALEX.explainer = DALEX.explainer
-          , DALEX.permutation.fi = DALEX.permutation.fi
-          , DALEX.permutation.fi.plot = DALEX.permutation.fi.plot
-        )
-      )
-    })
-  # 44s B=10, 220s B=50, 408s B=100
-
-  return(fi.list.DALEX)
-}
-
-datasets.models.list %>% names
+################################################################################
+# Step2: get permutation feature importance for each models.list
+################################################################################
+NEW <- TRUE
+# NEW <- FALSE
 
 if (NEW) {
+  getOption("parallelly.fork.enable")
+  options(parallelly.fork.enable = TRUE)
+  # plan(multisession)
+  plan(multicore)
   system.time(
     datasets.permutation.fi.lists <- datasets.models.list %>%
-      map(~ get_permutation_fi_for_models_list(.x))
-  ) # 474s = 7.9min
+      map(~ get_pimp_for_models_list(.x, no_permutations = 50))
+  ) # 60s (B = 5) vs 474s (B = 50)
 
   system.time(
     datasets.permutation.fi.lists %>%
@@ -161,143 +198,101 @@ if (NEW) {
 
 }
 
-datasets.permutation.fi.lists
+datasets.permutation.fi.lists %>% names
 
-fi.list <- datasets.permutation.fi.lists$PERF10.big5composites.all
-fi.list <- datasets.permutation.fi.lists$PERF10.big5items.all
+datasets.permutation.fi.lists %>%
+  get_pimp_range_List_models_list("composites")
 
-fi.list$LR$DALEX.permutation.fi.plot
-fi.list$kNN$DALEX.permutation.fi.plot
-fi.list$GBM$DALEX.permutation.fi.plot
-fi.list$RF$DALEX.permutation.fi.plot
-fi.list$SVM$DALEX.permutation.fi.plot
+datasets.permutation.fi.lists %>%
+  get_pimp_range_List_models_list("items")
 
-#####################################################
-# create feature importance tables & plots
-#####################################################
-# step1: extract varImp-able models from each models.list
-models.varimp.list <- datasets.models.list %>%
-  # tricky: start with .x
-  map( ~ .x %>% get_models_varimp() )
+################################################################################
+# Step3: save feature importance plots
+################################################################################
+save_pimp_plots <- function(
+  datasets_pimp_lists,
+  model_string = "",
+  scaling_factor = 1,
+  width = "automatic",
+  height = "automatic",
+  axis_tick_labels = NULL) {
 
-# select model label
-model.label.publish <- "GBM"
+  pimp.max <- datasets_pimp_lists %>%
+    get_pimp_range_List_models_list(model_string) %>%
+    max %>%
+    print
 
-# # try single model
-# models.varimp.list$PERF10.big5composites.all %>%
-#   pluck(model.label.publish) %>%
-#   visualize_importance(relative = TRUE, labels = TRUE)
+  pimp.min <- datasets_pimp_lists %>%
+    get_pimp_range_List_models_list(model_string, min) %>%
+    min %>%
+    print
 
-# step2: create varimp plots+tables for each models.list
-varimp.list <- models.varimp.list %>%
-  map(~ .x %>% # .x = models.list
-        map( ~ visualize_importance(
-          .x, relative = TRUE, labels = TRUE
-        )))
-
-# get varImp plots - only for model.label.publish
-varimp.list %>% map(
-  ~ .x %>%
-    pluck(model.label.publish) %>%
-    pluck("importance.plot") )
-
-# for BigFive Factors (oo, cc, ee, aa, nn)
-# options(digits = 3)
-varimp.list$PERF10.big5composites.all$GBM
-varimp.list$`PERF10.big5composites.R&D`$GBM
-varimp.list$PERF10.big5composites.sales$GBM
-varimp.list$PERF10.big5composites.support$GBM
-
-# for BigFive Facets (oo1~5, cc1~5, ee1~5, aa1~5, nn1~5)
-# options(digits = 2)
-varimp.list$PERF10.big5items.all$GBM
-varimp.list$`PERF10.big5items.R&D`$GBM
-varimp.list$PERF10.big5items.sales$GBM
-varimp.list$PERF10.big5items.support$GBM
-
-# step3: save varImp plots
-# # this works - but does not consider axis_limits
-# map2(
-#   varimp.list, names(varimp.list),
-#   ~ .x %>%
-#     pluck(model.label.publish) %>%
-#     pluck("importance.plot") %>%
-#     ggsave(
-#       filename =  paste0(c("figures/importance",
-#                            .y, model.label.publish, "png"),
-#                          collapse = "."),
-#       plot = .,
-#       dpi = 300,
-#       width = 7, height = 3
-#     )
-# )
-
-# save varImp plots with different axis_limits for composites/items
-plot_varImp_list_of_lists <- function(dataset_type) {
-
-  if (dataset_type == "items") {
-
-    list.of.lists <-
-      get_listelements_by_string(models.varimp.list, "items")
-    width = 6
-    height = 6
-    axis_limit = 48 # items
-    axis_label = "facet"
-
-  } else if (dataset_type == "composites") {
-
-    list.of.lists <-
-      get_listelements_by_string(models.varimp.list, "composites")
-    width = 7
-    height = 3
-    axis_limit = 103 # composites
-    axis_label = "factor"
+  no.features <- function(model) {
+    model %>%
+      pluck("DALEX.explainer") %>%
+      pluck("data") %>%
+      ncol %>% print
   }
 
-  map2(list.of.lists, names(list.of.lists),
-
-       function(models_list, models_list_label) {
-
-         map2(models_list, names(models_list),
-
-              function(model_object, model_label) {
-
-                filename <- paste0(
-                  c("figures/importance",
-                    models_list_label, model_label, "png"),
-                  collapse = ".") %>% print
-
-                visualize_importance(
-                  model_object,
-                  relative = TRUE,
-                  text_labels = TRUE,
-                  axis_label = axis_label,
-                  axis_tick_labels = data.labels.long,
-                  save_label = filename,
-                  width = width,
-                  height = height,
-                  axis_limit = axis_limit
-                )
+  datasets_pimp_lists %>%
+    get_list_elements_by_string(model_string) %>%
+    imap(function(model, model_label) {
+      model %>%
+      imap(
+        ~ .x %>%
+          {
+            .$DALEX.permutation.fi.plot +
+              # same scale on flipped x-asis for same datasets_pimp_lists
+              scale_y_continuous(limits = c(pimp.min, pimp.max)) +
+              # next layer must be added within code block of plot object
+              {
+                if (!is.null(axis_tick_labels)) {
+                  scale_x_discrete(labels = axis_tick_labels)
+                }
               }
-         )
-       }
-  )
+          } %>%
+          ggsave(
+            filename = paste0(c("figures/pimp", model_label, .y, "png"), collapse = "."),
+            plot = .,
+            scale = scaling_factor,
+            width = ifelse(is.numeric(width), width, log(no.features(.x)) * 3),
+            height = ifelse(is.numeric(height), height, log(no.features(.x)) * 2.2),
+            dpi = 150
+          )
+      )
+    })
 }
 
 
-# change axis labels for factors/facets
-data.labels.long
-varimp.list$PERF10.big5items.all$GBM$importance.plot +
-  scale_x_discrete(labels = data.labels.long)
+# save feature importance plots
+if (NEW) {
 
-get_listelements_by_string(models.varimp.list, "composites")
-get_listelements_by_string(models.varimp.list, "items")
+  datasets.permutation.fi.lists %>%
+    save_pimp_plots("composites",
+                    scaling_factor = 1.3,
+                    width = 4,
+                    height = 3,
+                    axis_tick_labels = data.labels.long)
 
-plot_varImp_list_of_lists("composites")
-plot_varImp_list_of_lists("items")
+  system.time(
+    datasets.permutation.fi.lists %>%
+      save_pimp_plots("items",
+                      scaling_factor = 0.9,
+                      width = 9,
+                      height = 8,
+                      axis_tick_labels = data.labels.long)
+  )
+}
+# fi.list <- datasets.permutation.fi.lists$PERF10.big5composites.all
+fi.list <- datasets.permutation.fi.lists$PERF10.big5items.all
 
+fi.list$LR$DALEX.permutation.fi
+fi.list$LR$DALEX.permutation.fi %>% as_tibble
+fi.list$LR$DALEX.permutation.fi %>% attributes
+fi.list$LR$DALEX.permutation.fi %>% names
 
-
+datasets.permutation.fi.lists %>%
+  get_pimp_range_List_models_list("items", min)
 
 
 
