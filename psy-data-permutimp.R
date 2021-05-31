34dfc0a27934a69970c152c16da9e1ee65f06ad2
Chaehan So <chaehan.so@gmail.com>
2021-06-01 03:25:32 +0900

    moved functions to machinelearningtools::xai.R, replaced map() by future_map()

--- psy-data-permutimp.R
+++ psy-data-permutimp.R
@@ -5,11 +5,10 @@
 #
 ################################################################################
 # load libraries
-# detach("package:machinelearningtools", character.only = TRUE)
+detach("package:machinelearningtools", character.only = TRUE)
 # devtools::install_github("agilebean/machinelearningtools", force = TRUE)
 # library(credentials)
 # set_github_pat(force_new = FALSE, validate = interactive(), verbose = TRUE)
-
 libraries <- c(
   "magrittr"
   , "caret"
@@ -22,124 +21,11 @@ libraries <- c(
   , "furrr"
 )
 sapply(libraries, require, character.only = TRUE)
+furrr_options(seed = TRUE)
 
 source("_labels.R")
 source("_common.R")
 
-getOption("digits")
-
-################################################################################
-# get permutation feature importance (pimp) for models.list
-################################################################################
-get_pimp_for_models_list <- function(
-  models_list, no_permutations = 50, seed = 171) {
-
-
-
-  fi.list.DALEX <- models_list %>%
-
-    imap(function(model_object, model_name) {
-
-      # model_object <- models.list$SVM
-      print(paste("*********", model_object$method))
-
-      training.set <- model_object$trainingData %>%
-        select(.outcome, everything())
-
-      target <- training.set$.outcome
-      print(paste("***target"))
-
-      features <- training.set %>% select(-.outcome)
-
-      DALEX.explainer <- DALEX::explain(
-        model = model_object,
-        data = features,
-        y = target,
-        label = model_name, # .y in imap() is list element name
-        colorize = TRUE
-      )
-      print("*** DALEX.explainer")
-
-      set.seed(seed)
-      system.time(
-        DALEX.permutation.fi <- model_parts(
-          explainer = DALEX.explainer,
-          loss_function = loss_root_mean_square,
-          B = no_permutations,
-          type = "ratio"
-        )
-      ) # 78-84s for rf
-
-      print("*** DALEX.permutation.fi")
-
-      # bar_width = 5.9 items / 12.4 composites
-      ( bar.width <- 20 / log(ncol(features)) )
-
-      DALEX.permutation.fi.plot <- DALEX.permutation.fi %>%
-        plot(
-          bar_width = bar.width
-          , show_boxplots = FALSE
-          , title = "Permutation Feature importance"
-          , subtitle = ""
-        ) +
-        scale_y_continuous(expand = expansion()) # reduce space to axis
-
-      print("*** DALEX.permutation.fi.plot")
-
-      return(
-        list(
-          DALEX.explainer = DALEX.explainer
-          , DALEX.permutation.fi = DALEX.permutation.fi
-          , DALEX.permutation.fi.plot = DALEX.permutation.fi.plot
-        )
-      )
-    })
-  # 44s B=10, 220s B=50, 408s B=100
-
-  return(fi.list.DALEX)
-}
-
-#####################################################
-# calculate permutation feature importance
-#####################################################
-get_pimp_range_List_models_list <- function(
-  list_models_list, model_string = "", value_function = max) {
-
-  list_models_list %>%
-    {
-      if (model_string != "") {
-
-        get_list_elements_by_string(., model_string)
-
-      } else {
-        .
-      }
-    } %>%
-    {
-      # TRICKY: temporary assignment works only for . not names(.) bec it
-      # is piped through, so call models.selected %>% names later
-      {
-        . -> models.selected
-      } %>%
-        imap_dfr(function(models_list, models_list_label) {
-          models_list %>%
-            imap(function(model, model_label) {
-              model$DALEX.permutation.fi %>%
-                filter(variable != "_baseline_") %>%
-                group_by(variable) %>%
-                summarise(mean_dropout_loss = mean(dropout_loss)) %>%
-                .$mean_dropout_loss %>%
-                value_function
-            })
-        }) %>%
-        as.data.frame %>%
-      set_rownames(models.selected %>% names)
-    }
-}
-
-
-################################################################################
-# MAIN
 ################################################################################
 # Step1) read models.lists from all datasets
 ################################################################################
@@ -174,19 +60,33 @@ NEW <- TRUE
 # NEW <- FALSE
 
 if (NEW) {
+  future::supportsMulticore()
+
   getOption("parallelly.fork.enable")
   options(parallelly.fork.enable = TRUE)
-  # plan(multisession)
-  plan(multicore)
+
+  plan(multicore (workers = 8))
+
   system.time(
     datasets.permutation.fi.lists <- datasets.models.list %>%
-      map(~ get_pimp_for_models_list(.x, no_permutations = 50))
-  ) # 60s (B = 5) vs 499s (B = 50)
+      future_map(
+        ~ get_pimp_for_models_list(.x, no_permutations = 100),
+        .options = furrr_options(
+          seed = TRUE
+          , packages = c("dplyr", "DALEX")
+        )
+      ) %T>% {
+        beepr::beep()
+      }
+  )
+# B = 5: 119s
+# B = 50: 315.2s multicore8  vs. 499-621s, map
+# B = 100: 667s multicore8 vs. 1146s map
 
   system.time(
     datasets.permutation.fi.lists %>%
       saveRDS(permutation.fi.lists.label)
-  ) # 71s
+  ) # 71s (B50) 84s (B100)
 
 } else {
 
@@ -205,64 +105,10 @@ datasets.permutation.fi.lists %>%
 datasets.permutation.fi.lists %>%
   get_pimp_range_List_models_list("items")
 
+
 ################################################################################
 # Step3: save feature importance plots
 ################################################################################
-save_pimp_plots <- function(
-  datasets_pimp_lists,
-  model_string = "",
-  scaling_factor = 1,
-  width = "automatic",
-  height = "automatic",
-  axis_tick_labels = NULL) {
-
-  pimp.max <- datasets_pimp_lists %>%
-    get_pimp_range_List_models_list(model_string) %>%
-    max %>%
-    print
-
-  pimp.min <- datasets_pimp_lists %>%
-    get_pimp_range_List_models_list(model_string, min) %>%
-    min %>%
-    print
-
-  no.features <- function(model) {
-    model %>%
-      pluck("DALEX.explainer") %>%
-      pluck("data") %>%
-      ncol %>% print
-  }
-
-  datasets_pimp_lists %>%
-    get_list_elements_by_string(model_string) %>%
-    imap(function(model, model_label) {
-      model %>%
-      imap(
-        ~ .x %>%
-          {
-            .$DALEX.permutation.fi.plot +
-              # same scale on flipped x-asis for same datasets_pimp_lists
-              scale_y_continuous(limits = c(pimp.min, pimp.max)) +
-              # next layer must be added within code block of plot object
-              {
-                if (!is.null(axis_tick_labels)) {
-                  scale_x_discrete(labels = axis_tick_labels)
-                }
-              }
-          } %>%
-          ggsave(
-            filename = paste0(c("figures/pimp", model_label, .y, "png"), collapse = "."),
-            plot = .,
-            scale = scaling_factor,
-            width = ifelse(is.numeric(width), width, log(no.features(.x)) * 3),
-            height = ifelse(is.numeric(height), height, log(no.features(.x)) * 2.2),
-            dpi = 150
-          )
-      )
-    })
-}
-
-
 # save feature importance plots
 if (NEW) {
 
